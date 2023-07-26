c5b8a4656c44ffe893ab3e08a82910ee2434625c
keisuke-333 <keikubo.999@gmail.com>
2023-07-27 00:56:11 +0900

    fix Head component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import "@/styles/globals.css"
 
 import { type AppType } from "next/app"
+import Head from "next/head"
 
 import { api } from "@/utils/api"
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
+const MyApp: AppType = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>todo-app</title>
+      <meta name="description" content="This is a simple todo app" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+)
 
 export default api.withTRPC(MyApp)
