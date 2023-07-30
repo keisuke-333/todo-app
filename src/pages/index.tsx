058d6fc1f998522951d8241651e35d907ad7520a
keisuke-333 <keikubo.999@gmail.com>
2023-07-30 13:01:18 +0900

    pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,8 @@ const HomePage = () => {
   const [editText, setEditText] = useState("")
   const [filter, setFilter] = useState("all")
   const [search, setSearch] = useState("")
+  const [currentPage, setCurrentPage] = useState(1)
+  const itemsPerPage = 5
 
   const { isLoading, error, data, refetch } = api.todo.fetch.useQuery()
 
@@ -94,6 +96,13 @@ const HomePage = () => {
       deleteMutation.mutateAsync({ id }).catch((error) => {
         console.error("An error occurred during deletion: ", error)
       })
+      if (currentItems?.length === 1) {
+        if (currentPage > 1) {
+          setCurrentPage((oldPage) => oldPage - 1)
+        } else {
+          setCurrentPage(1)
+        }
+      }
     }
   }
 
@@ -115,6 +124,12 @@ const HomePage = () => {
   if (search !== "") {
     filteredData = filteredData?.filter((todo) => todo.title.includes(search)) ?? []
   }
+
+  const numPages = Math.ceil((filteredData?.length ?? 0) / itemsPerPage)
+  const currentItems = filteredData?.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage,
+  )
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
@@ -147,39 +162,85 @@ const HomePage = () => {
         </select>
       </div>
 
-      {filteredData?.map((todo) => (
-        <div
-          key={todo.id}
-          className="mb-2 flex w-[300px] cursor-pointer items-center border px-4 py-2 shadow hover:bg-slate-100"
-          onClick={() => handleStartEdit(todo.id, todo.title)}
-        >
-          <input
-            type="checkbox"
-            id="checkbox"
-            className="h-5 w-5 cursor-pointer"
-            checked={todo.isCompleted}
-            onClick={(e) => handleCheckboxClick(e, todo.id, todo.isCompleted)}
-          />
-          {isEditing === todo.id ? (
-            <input
-              type="text"
-              className="mx-4 w-[151px] p-1"
-              value={editText}
-              onChange={handleEditChange}
-              onBlur={() => handleEndEdit(todo.id)}
-              autoFocus={true}
-            />
-          ) : (
-            <p className={`mx-4 grow ${todo.isCompleted ? "text-gray-300" : ""}`}>{todo.title}</p>
-          )}
+      <div className="flex h-[360px] w-[300px] flex-col gap-2">
+        {(currentItems?.length ?? 0) > 0 ? (
+          currentItems?.map((todo) => (
+            <div
+              key={todo.id}
+              className="flex cursor-pointer items-center border px-4 py-2 shadow hover:bg-slate-100"
+              onClick={() => handleStartEdit(todo.id, todo.title)}
+            >
+              <input
+                type="checkbox"
+                id="checkbox"
+                className="h-5 w-5 cursor-pointer"
+                checked={todo.isCompleted}
+                onClick={(e) => handleCheckboxClick(e, todo.id, todo.isCompleted)}
+              />
+              {isEditing === todo.id ? (
+                <input
+                  type="text"
+                  className="mx-4 w-[151px] p-1"
+                  value={editText}
+                  onChange={handleEditChange}
+                  onBlur={() => handleEndEdit(todo.id)}
+                  autoFocus={true}
+                />
+              ) : (
+                <p className={`mx-4 grow ${todo.isCompleted ? "text-gray-300" : ""}`}>
+                  {todo.title}
+                </p>
+              )}
+              <button
+                className="rounded bg-red-500 p-2 text-white hover:bg-red-700"
+                onClick={() => handleDeleteClick(todo.id)}
+              >
+                Delete
+              </button>
+            </div>
+          ))
+        ) : (
+          <p className="text-center text-lg text-gray-500">No todos found.</p>
+        )}
+      </div>
+
+      <div className="mt-2">
+        <p>
+          {(filteredData?.length ?? 0) <= itemsPerPage
+            ? `${filteredData?.length} todo`
+            : `${currentPage === 1 ? 1 : (currentPage - 1) * itemsPerPage + 1} - ${
+                currentPage * itemsPerPage > (filteredData?.length ?? 0)
+                  ? filteredData?.length
+                  : currentPage * itemsPerPage
+              } of ${filteredData?.length}`}
+        </p>
+      </div>
+
+      {(filteredData?.length ?? 0) > itemsPerPage && (
+        <div className="mt-4 flex justify-center gap-2">
           <button
-            className="rounded bg-red-500 p-2 text-white hover:bg-red-700"
-            onClick={() => handleDeleteClick(todo.id)}
+            className={`rounded-l px-2 py-1 text-sm font-bold text-gray-800 ${
+              currentPage === 1 ? "cursor-not-allowed bg-gray-300" : "bg-gray-300 hover:bg-gray-400"
+            }`}
+            onClick={() => currentPage > 1 && setCurrentPage((oldPage) => Math.max(oldPage - 1, 1))}
+            disabled={currentPage === 1}
           >
-            Delete
+            Prev
+          </button>
+
+          <button
+            className={`rounded-r px-2 py-1 text-sm font-bold text-gray-800 ${
+              currentPage === numPages
+                ? "cursor-not-allowed bg-gray-300"
+                : "bg-gray-300 hover:bg-gray-400"
+            }`}
+            onClick={() => setCurrentPage((oldPage) => Math.min(oldPage + 1, numPages))}
+            disabled={currentPage === numPages}
+          >
+            Next
           </button>
         </div>
-      ))}
+      )}
     </main>
   )
 }
