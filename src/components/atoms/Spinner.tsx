70d191c23eda641da5cbff04379bbd20e0ba55a8
keisuke-333 <keikubo.999@gmail.com>
2023-08-01 17:24:34 +0900

    fix Spinner style props

--- src/components/atoms/Spinner.tsx
+++ src/components/atoms/Spinner.tsx
@@ -1,5 +1,13 @@
-export const Spinner = () => {
+type Props = {
+  margin?: string
+  height?: string
+  width?: string
+}
+
+export const Spinner = ({ margin = "my-5", height = "h-8", width = "w-8" }: Props) => {
   return (
-    <div className="my-5 h-8 w-8 animate-spin rounded-full border-2 border-pink-600 border-t-transparent"></div>
+    <div
+      className={`${margin} ${height} ${width} animate-spin rounded-full border-2 border-pink-600 border-t-transparent`}
+    ></div>
   )
 }
