367f8cc59a6b01f2648d2d149c0f216542330953
keisuke-333 <keikubo.999@gmail.com>
2023-08-02 17:46:09 +0900

    add delete button icon

--- src/components/atoms/TodoDeleteButton.tsx
+++ src/components/atoms/TodoDeleteButton.tsx
@@ -1,13 +1,15 @@
 import type { MouseEventHandler } from "react"
 
+import { DeleteIcon } from "./icons/DeleteIcon"
+
 type Props = {
   onClick: MouseEventHandler<HTMLButtonElement>
 }
 
 export const TodoDeleteButton = ({ onClick }: Props) => {
   return (
-    <button className="rounded bg-red-500 p-2 text-white hover:bg-red-700" onClick={onClick}>
-      Delete
+    <button className="rounded bg-red-500 p-1 text-white hover:bg-red-700" onClick={onClick}>
+      <DeleteIcon />
     </button>
   )
 }
