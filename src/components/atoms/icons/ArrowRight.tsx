ca58b9b9150aa621176062dc97bde9825da72da8
keisuke-333 <keikubo.999@gmail.com>
2023-08-02 17:45:40 +0900

    create icons component

---
+++ src/components/atoms/icons/ArrowRight.tsx
@@ -0,0 +1,5 @@
+export const ArrowRight = () => (
+  <svg viewBox="0 -960 960 960" width="24" height="24" aria-label="Arrow Right">
+    <path fill="black" d="M400-280v-400l200 200-200 200Z" />
+  </svg>
+)
